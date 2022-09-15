212a1b8681be6e194f09329879f1adfecfbd1554
Elaidzha1940 <eli080293@gmail.com>
2022-09-15 03:30:27 +0300

    18

---
+++ SwiftUI Segmented Control Tutorial/SwiftUI Segmented Control Tutorial/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  SwiftUI Segmented Control Tutorial
+//
+//  Created by Elaidzha Shchukin on 13.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .yellow
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.black], for: .selected)
+    }
+    
+    @State private var selectedSide: SideOfTheForce = .dark
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                
+                Picker("Choose a side", selection: $selectedSide) {
+                    ForEach(SideOfTheForce.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                Spacer()
+                ChosenPlayerView(selectedSide: selectedSide)
+                Spacer()
+            }
+            .navigationTitle("Choose a side")
+        }
+    }
+}
+
+enum SideOfTheForce: String, CaseIterable {
+    
+    case light = "Light"
+    case grey = "Grey"
+    case dark = "Dark"
+}
+
+
+struct ChosenPlayerView: View {
+    
+    var selectedSide: SideOfTheForce
+    
+    var body: some View {
+        switch selectedSide {
+        case .light:
+            PlayerImageView(playerName: "Kobe 1")
+                .cornerRadius(10)
+        case .grey:
+            PlayerImageView(playerName: "Kobe 0")
+                .cornerRadius(10)
+        case .dark:
+            PlayerImageView(playerName: "Kobe 2")
+                .cornerRadius(10)
+            
+        }
+    }
+}
+
+struct PlayerImageView: View {
+    
+    var playerName: String
+    
+    var body: some View {
+        Image(playerName)
+            .resizable()
+            .frame(width: 310, height: 530)
+            .shadow(color: .white, radius: 80)
+    }
+}
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ SwiftUI Segmented Control Tutorial/SwiftUI Segmented Control Tutorial/SwiftUI_Segmented_Control_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Segmented_Control_TutorialApp.swift
+//  SwiftUI Segmented Control Tutorial
+//
+//  Created by Elaidzha Shchukin on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Segmented_Control_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
